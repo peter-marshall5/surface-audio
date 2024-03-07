5d1d1bb1cc96d119787ee2b0edcb5512ae15be30
Peter Marshall <petms@proton.me>
2024-03-07 11:11:21 -0500

    Initial commit

---
+++ 85-surface-policy.lua
@@ -0,0 +1,11 @@
+surface = {
+  matches = {
+    {
+      { "media.class", "matches", "*/Sink" },
+      { "node.name", "=", "alsa_output.pci-0000_00_1f.3.analog-stereo" }
+    }
+  },
+  filter_chain = '/etc/surface-audio/graph.json',
+  hide_parent = true
+}
+table.insert(dsp_policy.policy.rules, surface)

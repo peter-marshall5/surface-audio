4685e784c2b5030cc9dd9d3250191105dccacc20
peter-marshall5 <78369157+peter-marshall5@users.noreply.github.com>
2024-03-18 16:30:03 -0400

    Update 85-surface-policy.lua

--- 85-surface-policy.lua
+++ 85-surface-policy.lua
@@ -5,7 +5,7 @@ surface = {
       { "node.name", "=", "alsa_output.pci-0000_00_1f.3.analog-stereo" }
     }
   },
-  filter_chain = '/etc/surface-audio/graph.json',
+  filter_chain = '/etc/wireplumber/surface-audio/graph.json',
   hide_parent = true
 }
 table.insert(dsp_policy.policy.rules, surface)
